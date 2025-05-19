8aaf4dcbb569f59f47a54dd8d79447dc90462154
TheDidEv <thedidyev@gmail.com>
2025-05-20 01:17:36 +0300

    update

--- server/schemas/auth.py
+++ server/schemas/auth.py
@@ -2,9 +2,21 @@ from pydantic import BaseModel, Field
 
 
 class CreateUserRequest(BaseModel):
-    username: str = Field(min_length=2, max_length=25)
-    email: str = Field(min_length=6, max_length=100)
-    password: str
+    username: str = Field(min_length=2, max_length=25, description="Username (2-25 characters)")
+    email: str = Field(min_length=6, max_length=100, description="Email (6-100 characters)")
+    password: str = Field(min_length=6, max_length=128, description="Password (6-128 characters)")
+    
+    model_config = {
+        "json_schema_extra": {
+            "title": "Request to create a user",
+            "description": "Auth model",
+            "example": {
+                "username": "John",
+                "email": "john@gmail.com",
+                "password": "password",
+            }
+        }
+    }
     
 class Token(BaseModel):
     access_token: str
